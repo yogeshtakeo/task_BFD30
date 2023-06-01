b819b1dce8e030dadc958f65d9dedf2f9911ee6c
babisha <mdr.bebo@gmail.com>
2023-06-01 13:41:12 -0400

    Task:9 Handling Promise Final

--- folder_structure_and_js_concepts/api.js
+++ folder_structure_and_js_concepts/api.js
@@ -1,45 +1,21 @@
-// function resolveATask2Seconds() {
-//     setTimeout(() => {
-//         console.log("Response 1");
+// error in fetch witha typo in the url
 
-//     }, 2000)
-// }
+const fetchingTheData = fetch("htts://jsonplaceholder.typicode.com/comments");
+console.log(fetchingTheData)
 
-// const promiseResolved = new Promise((resolve, reject) => {
-//     resolve("Hello this is the response")
-// });
-
-// const promiseRejected = new Promise((resolve, reject) => {
-//     reject("This is rejected Promise");
-
-// });
-
-// promiseResolved.then((response) => {
-//     console.log(response);
-// })
-// .catch((error) =>{
-//     console.log("This is an error", error.repsonse);
-// })
-
-// const fetchingTheData = window.fetch("https://jsonplaceholder.typicode.com/todos/1");
-// console.log(fetchingTheData);
-
-// fetchingTheData
-//     .then((response) => response.json())
-//     .then((json) => {
-//         console.log(json);
-//     })
-//     .catch((error) => {
-//         console.log("This is an error", error);
-//     })
+fetchingTheData
+    .then((response) => response.json())
+    .then((json) => {
+        console.log(json);
+    })
+    .catch((error) => {
+        console.log("This is an absolute error", error);
+    })
 
 
+// Promise handled with correct url
 
-// console.log("promise Resolved", promiseResolved);
-// console.log("promise Rejected", promiseRejected);
-// resolveATask2Seconds();
-
-const fetchingTheData = fetch("https://jsonplaceholder.typicode.com/comments");
+    const fetchingTheData = fetch("https://jsonplaceholder.typicode.com/comments");
 console.log(fetchingTheData)
 
 fetchingTheData

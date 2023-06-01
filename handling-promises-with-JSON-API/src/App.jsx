2630d6778a17502b8d80932cad7337e40130896c
Kabina Thapa <thapakabina28@gmail.com>
2023-06-01 15:13:18 -0400

    Handling promises with then() and catch() methods.

---
+++ handling-promises-with-JSON-API/src/App.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+
+function App() {
+  //1. Make API request and handle promise
+
+  //Fetch function to make a GET request to chosen API endpoint.
+  const dataFetch = fetch("https://jsonplaceholder.typicode.com/posts");
+  console.log(dataFetch);
+
+  //Handling the promise using then() and catch() methods; hanldes the response or any errors that may occur.
+
+  //The 1st then() method is called when promise is resolved successfully,i.e; server responded with valid HTTP response.
+
+  dataFetch
+    .then((response) => response.json()) //response.json() method is called to parse response body as JSON. It returns a promise that resolves to parsed JSON data.
+
+    .then((data) => { //The 2nd then() method is chained to promise returned by response.json().
+      console.log(data);
+    })
+
+    //In case of any errors that occur during fetch operation or request, the catch() method is called.
+
+    .catch((error) => {
+      console.error("Error", error);
+    });
+  }

7f7a10485ca07856398215745d0438558d5608ff
Kabina Thapa <thapakabina28@gmail.com>
2023-06-02 23:43:58 -0400

    Data fetching and handling the promise using async-await method

--- todo-application/src/App.jsx
+++ todo-application/src/App.jsx
@@ -1,33 +1,43 @@
-import { useState } from 'react'
+import React,{ useState,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+//useState implementation for updating state of task and error.
+  const[task, setTask]=useState({})
+  const[error, setError]=useState('')
+//functional Component
+  const TodoApp = () => {
+    //Useeffect to handle the re-rendering
+    useEffect(()=>{
+    const addTask= async ()=>{
+      //handling the promise using async-await and trycatch.
+      try{
+        const jsonObject = await fetch("https://jsonplaceholder.typicode.com/todos/")
+        const jsObject= await jsonObject.json()
+        setTask(jsObject)
+      }
+      catch (error) {
+        setError('Something went wrong!')
+        
+        
+      }
+   }
+    TodoApp()
+  },[])
 
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {error ? (
+        <h1>{error}</h1>
+        ):
+        (<div className="outline">
+          <button>Task</button>
+          <h1>{task.userId}</h1>
+        </div>)
+      }
     </>
   )
 }

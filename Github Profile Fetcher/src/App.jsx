70f2316a02b27e2da1d106517185e5bc518a0f1f
babisha <mdr.bebo@gmail.com>
2023-06-08 17:30:01 -0400

    'Fetching Data and rendering on Local Host'

--- Github Profile Fetcher/src/App.jsx
+++ Github Profile Fetcher/src/App.jsx
@@ -3,40 +3,40 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-// // function App() {
-// //   const [state, setState] = useState({})
-// //   const [error, setError] = useState('')
+function App() {
+  const [state, setState] = useState({})
+  const [error, setError] = useState('')
 
-// //   useEffect(() => {
-// //     const getGithubInfo = async () => {
-// //       try{
-// //         const jsonObject = await fetch("https://api.github.com/users/babisha")
-// //         const jsObject = await jsonObject.json()
+  useEffect(() => {
+    const getGithubInfo = async () => {
+      try{
+        const jsonObject = await fetch("https://api.github.com/users/babisha")
+        const jsObject = await jsonObject.json()
 
-// //         setState(jsObject)
-// //          } catch(error) {
-// //       setError('You got an error, my friend!')
-// //          }
-// //     }
+        setState(jsObject)
+         } catch(error) {
+      setError('You got an error, my friend!')
+         }
+    }
 
-// //     getGithubInfo()
+    getGithubInfo()
 
-// //   }, [])
+  }, [])
 
 
   
 
-// //   return (
-// //     <>
-// //       <div className="wrapper">
-// //          <div className='card'>
-// //             <h2>GITHUB : <button onclick = {state.url}>{state.login}</button></h2>
-// //             <img className='babisha' src={state.avatar_url}/>
-// //             <h3>{state.login} has about {state.followers} followers. She started her coding journey in {state.location}.</h3>
-// //         </div>
-// //       </div>
-// //     </>
-//   )
-// }
+  return (
+    <>
+      <div className="wrapper">
+         <div className='card'>
+            <h2>GITHUB : <button onclick = {state.url}>{state.login}</button></h2>
+            <img className='babisha' src={state.avatar_url}/>
+            <h3>{state.login} has about {state.followers} followers. She started her coding journey in {state.location}.</h3>
+        </div>
+      </div>
+    </>
+  )
+}
 
 export default App

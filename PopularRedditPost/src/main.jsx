fd09cb3b421c6e3f04b010c0820d576da25d4ba4
JunuThapa <junuthapa011@gmail.com>
2023-06-09 18:15:50 -0400

    Task13: Initial commit

--- PopularRedditPost/src/main.jsx
+++ PopularRedditPost/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import RedditPostViewer from './assets/RedditPostViewer'
+import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RedditPostViewer />
+    <App />
   </React.StrictMode>,
 )

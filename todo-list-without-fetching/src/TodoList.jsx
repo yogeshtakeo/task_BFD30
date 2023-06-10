c57689d22423e0a6c380d1af5e3704ba5e3c5109
Kabina Thapa <thapakabina28@gmail.com>
2023-06-10 18:41:56 -0400

    Handling delete Task in a list

--- todo-list-without-fetching/src/TodoList.jsx
+++ todo-list-without-fetching/src/TodoList.jsx
@@ -2,24 +2,34 @@ import React from 'react'
 import {useState } from 'react'
 
 
+
 const TodoList = () => {
     //useState to update list with new tasks.
     const[list, setList]=useState([])
     //useState to update newTask
     const[newTask, setNewTask]=useState('')
-
+    //eventhandler onChange
     const handleChange=(event)=>{
         setNewTask(event.target.value)
     }
-
+    //addTask to update the list with newTask onCick
     const addTask=()=>{
         const tasks={id:list.length+1, title:newTask, completed:false}
+        //merge the list with the newTask using spread operator
         setList([...list, tasks])
-        
+        //setNewTask updates state of newTask to null after the prev newTask is submited onClick
         setNewTask('')
-        
     }
-    console.log(list)
+    const deleteTask=(id)=>{
+        //filter() filters the id and separates the ids which are not equal to the id clicked by user to be deleted.
+        const newArray=list.filter((items)=>items.id!==id)
+        //updates the List with newArray which excludes the id passed to be deleted.
+        setList(newArray)
+    }
+    const editTask=()=>{
+
+    }
+    
 
   return (
     <>
@@ -29,9 +39,13 @@ const TodoList = () => {
      
        {list?.map((lists)=>(
        <li key={lists.id}>
-        {lists.id}
+        {lists.id} 
         {lists.title}
+        <button onClick={()=>deleteTask(lists.id)}>
+            X
+        </button>
         </li>))}
+       
      
      </>
   )

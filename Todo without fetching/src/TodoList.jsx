e760117d84d54c94fc67e7920636f1bc1b5b4ad3
babisha <mdr.bebo@gmail.com>
2023-06-09 23:40:40 -0400

    Initial Commit

---
+++ Todo without fetching/src/TodoList.jsx
@@ -0,0 +1,29 @@
+
+function arrayElements(todo) {
+    return (
+        <>
+            <div className="box">Check The ToDo Lists</div>
+            <div className= "main">
+                <h2>Id = {todo.id}</h2>
+                <h2>Title = {todo.title}</h2>
+                <h2>Task Completion = {todo.isCompleted}</h2>
+
+            </div>
+        
+        
+        
+        
+        
+        
+        </>
+        
+       
+        
+        
+        
+        
+        
+    )
+}
+
+export default arrayElements;

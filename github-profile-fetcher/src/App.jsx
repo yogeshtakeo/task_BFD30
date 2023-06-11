18db2001ce5538fbe9a2a3691cbfb61e0596d92b
Kabina Thapa <thapakabina28@gmail.com>
2023-06-11 19:31:44 -0400

    Task 10: Completed

--- github-profile-fetcher/src/App.jsx
+++ github-profile-fetcher/src/App.jsx
@@ -4,17 +4,7 @@ import './App.css'
 
  function App() {
  
-  //Handling promises with .then() and .catch() methods
-  //const profile= fetch('https://api.github.com/users/kabinathapa')
-    //console.log(profile)
-    //profile
-    //.then((response)=>response.json())
-    //.then((data)=>{
-      //console.log(data)
-    //})
-    //.catch((error)=>{
-      //console.log("Error", error)
-    //})
+ 
 
   return (
     <>

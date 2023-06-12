fc8fe39a3bdb5b7a956a0540acc989661678a55a
Jharanatmg <jharanatmg22@gmail.com>
2023-06-11 23:04:45 -0700

    Task 11: Todo fetched

--- todo_app/src/App.jsx
+++ todo_app/src/App.jsx
@@ -4,12 +4,13 @@ import TodoApp from './todo'
 function App() {
   
   return (
-    <>
-    
+    <div className='box'>
+
+       <h1>To-do Lists</h1>
+
       <TodoApp />
-     
-     
-    </>
+         
+    </div>
   )
 }
 
--- todo_app/src/todo.jsx
+++ todo_app/src/todo.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react'
-
+import './App.css'
 
 function TodoApp() {
     const [state, setState] = useState([])
@@ -20,12 +20,27 @@ function TodoApp() {
     }, [])
   
   return (
-    <div>
-      {todo.map((task) => (
+    <div className='card'>
+      <div className='container'>
+      <input className='bar' type='text' placeholder= '  Add new tasks'/> 
+      <button>Add</button>    
+      </div>
+      {state.map((task) => (
         <div className='list' key={task.id}> 
+       
         <ul>
           <li>{task.id}</li>
           <li>{task.title}</li>
+          
+          <li> <div className='check'>
+                    
+                <input type="checkbox" />
+                  <link
+                    href="https://fonts.googleapis.com/icon?family=Material+Icons"
+                    rel="stylesheet"></link>
+                  <i class="material-icons">delete</i>
+           </div> </li>    
+         
         </ul>
         </div>
       ))

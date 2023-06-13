5ed6314a92765ad98780f8dfe95f31cb11667684
Ashma Upreti <ashma.upreti99@gmail.com>
2023-06-13 18:18:47 -0400

    Task 14: Added add and Delete functionalities to the app

--- Todo_without_fetch/src/Todo.jsx
+++ Todo_without_fetch/src/Todo.jsx
@@ -1,17 +1,34 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
+import "./App.css";
 
 function Todo() {
   //this usestate is to add new task
   const [newItem, setNewItem] = useState("");
   //array to hold all the new added task
   const [items, setItems] = useState([]);
+
   //function that is called when add button is clicked
   function Addtask() {
+    //create unique id using uuidv
     const item = {
-      id = uuidv4();
+      id: uuidv4(),
+      value: newItem,
     };
+    //adding new item to the old todo list.
+    setItems((oldList) => [...oldList, item]);
+    //making the new item empty again
+    setNewItem("");
+    console.log(items);
+  }
+
+  //funtion to call to delete task
+  function Deletetask(id) {
+    //creating new array which contains every task except the deleted
+    let newArray = items.filter((item) => item.id !== id);
+    //setting the value of items with new array which does not have
+    setItems(newArray);
   }
 
   return (
@@ -26,11 +43,19 @@ function Todo() {
       <button className="button" onClick={Addtask}>
         Add Task
       </button>
-      <div>
-        <ul>
-          <li>cook </li>
-        </ul>
-      </div>
+
+      <ul>
+        {items.map((item) => {
+          return (
+            <>
+              <li key={item.id}>
+                {item.value}
+                <button onClick={() => Deletetask(item.id)}>Delete</button>
+              </li>
+            </>
+          );
+        })}
+      </ul>
     </>
   );
 }

87d7c89640ce407d50bd7554bed7d3a7c3d4d418
Ashma Upreti <ashma.upreti99@gmail.com>
2023-06-17 01:16:37 -0400

    Task 14: Css implemented

--- Todo_without_fetch/src/Todo.jsx
+++ Todo_without_fetch/src/Todo.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 import "./App.css";
+import backgroundImage from "./assets/bg.jpg";
 
 function Todo() {
   //this usestate is to add new task
@@ -70,45 +71,59 @@ function Todo() {
 
   return (
     <>
-      <h1>Todo Application</h1>
-      <input
-        type="text"
-        placeholder="Enter the task"
-        onChange={InputHandler}
-        value={newItem}
-        // onChange={(e) => setNewItem(e.target.value)}
-      />
-      <button
-        className="button"
-        onClick={editTodo ? () => HandleUpdate(edit) : Addtask}>
-        {editTodo ? "Update" : "Add todo"}
-      </button>
+      <div className="flex items-center justify-center h-screen">
+        <div
+          className="bg-local bg-contain bg-center h-screen w-6/12 "
+          style={{ backgroundImage: `url(${backgroundImage})` }}>
+          <div className="flex flex-col items-center justify-center h-screen">
+            <h1>Todo Application</h1>
+            <input
+              className="w-64 h-10 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg pl-5"
+              type="text"
+              placeholder="Enter the task"
+              onChange={InputHandler}
+              value={newItem}
+              // onChange={(e) => setNewItem(e.target.value)}
+            />
+            <button
+              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+              onClick={editTodo ? () => HandleUpdate(edit) : Addtask}>
+              {editTodo ? "Update" : "Add todo"}
+            </button>
 
-      <ul>
-        {items.map((item) => {
-          return (
-            <>
-              <li key={item.id}>
-                <input
-                  type="checkbox"
-                  onChange={() => ToggleComplete(item.id)}
-                  checked={item.isCompleted}
-                />
-                {item.value}
-                <button onClick={() => Deletetask(item.id)}>Delete</button>
-                <button
-                  onClick={() => {
-                    setNewItem(item.value);
-                    setEditTodo(true);
-                    setEdit(item.id);
-                  }}>
-                  Edit
-                </button>
-              </li>
-            </>
-          );
-        })}
-      </ul>
+            <ul>
+              {items.map((item) => {
+                return (
+                  <>
+                    <li key={item.id}>
+                      <input
+                        type="checkbox"
+                        onChange={() => ToggleComplete(item.id)}
+                        checked={item.isCompleted}
+                      />
+                      {item.value}
+                      <button
+                        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                        onClick={() => Deletetask(item.id)}>
+                        Delete
+                      </button>
+                      <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                        onClick={() => {
+                          setNewItem(item.value);
+                          setEditTodo(true);
+                          setEdit(item.id);
+                        }}>
+                        Edit
+                      </button>
+                    </li>
+                  </>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
     </>
   );
 }

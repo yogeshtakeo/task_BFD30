1c94dff35a8b3d7b95c1e9322238336b686c0550
Ashma Upreti <ashma.upreti99@gmail.com>
2023-06-17 22:18:22 -0400

    Task 14: React Icons and Css Added

--- Todo_without_fetch/src/Todo.jsx
+++ Todo_without_fetch/src/Todo.jsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 import "./App.css";
 import backgroundImage from "./assets/bg.jpg";
+import { MdDeleteOutline } from "react-icons/md";
+import { AiOutlineEdit } from "react-icons/ai";
 
 function Todo() {
   //this usestate is to add new task
@@ -76,51 +78,62 @@ function Todo() {
           className="bg-local bg-contain bg-center h-screen w-6/12 "
           style={{ backgroundImage: `url(${backgroundImage})` }}>
           <div className="flex flex-col items-center justify-center h-screen">
-            <h1>Todo Application</h1>
-            <input
-              className="w-64 h-10 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg pl-5"
-              type="text"
-              placeholder="Enter the task"
-              onChange={InputHandler}
-              value={newItem}
-              // onChange={(e) => setNewItem(e.target.value)}
-            />
-            <button
-              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-              onClick={editTodo ? () => HandleUpdate(edit) : Addtask}>
-              {editTodo ? "Update" : "Add todo"}
-            </button>
+            <h1 className="font-bold text-2xl">To Do Application</h1>
+            <div className="flex mt-4">
+              <input
+                className="w-64 h-10 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg mr-2 pl-5"
+                type="text"
+                placeholder="Enter the task"
+                onChange={InputHandler}
+                value={newItem}
+                // onChange={(e) => setNewItem(e.target.value)}
+              />
+              <button
+                className="bg-white h-10 border-2 border-gray-600 hover:bg-white text-black font-bold py-2 px-4 rounded-lg"
+                onClick={editTodo ? () => HandleUpdate(edit) : Addtask}>
+                {editTodo ? "Update" : "Add To Do"}
+              </button>
+            </div>
+            <div className="grid">
+              <ul className="list-disc">
+                {items.map((item) => {
+                  return (
+                    <>
+                      <li className="flex items-center mr-2" key={item.id}>
+                        <div className="w-80 flex justify-between mt-2">
+                          <div>
+                            <input
+                              className="mr-2"
+                              type="checkbox"
+                              onChange={() => ToggleComplete(item.id)}
+                              checked={item.isCompleted}
+                            />
+                            {item.value}
+                          </div>
 
-            <ul>
-              {items.map((item) => {
-                return (
-                  <>
-                    <li key={item.id}>
-                      <input
-                        type="checkbox"
-                        onChange={() => ToggleComplete(item.id)}
-                        checked={item.isCompleted}
-                      />
-                      {item.value}
-                      <button
-                        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-                        onClick={() => Deletetask(item.id)}>
-                        Delete
-                      </button>
-                      <button
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                        onClick={() => {
-                          setNewItem(item.value);
-                          setEditTodo(true);
-                          setEdit(item.id);
-                        }}>
-                        Edit
-                      </button>
-                    </li>
-                  </>
-                );
-              })}
-            </ul>
+                          <div className="flex mt-2">
+                            <MdDeleteOutline
+                              className="mr-2"
+                              // className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                              onClick={() => Deletetask(item.id)}
+                            />
+
+                            <AiOutlineEdit
+                              // className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                              onClick={() => {
+                                setNewItem(item.value);
+                                setEditTodo(true);
+                                setEdit(item.id);
+                              }}
+                            />
+                          </div>
+                        </div>
+                      </li>
+                    </>
+                  );
+                })}
+              </ul>
+            </div>
           </div>
         </div>
       </div>
